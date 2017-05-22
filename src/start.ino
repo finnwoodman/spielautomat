f39a54b930c49708735d65ee80c48289138279c2
Max von Elverfeldt <m.elverfeldt@gmail.com>
2017-05-22 14:06:43 +0200

    README.md

--- src/start.ino
+++ src/start.ino
@@ -1,12 +1,16 @@
+#include <Button.h>
 #include <Rotary.h>
 
  Rotary rotary(0,1);
+ Button button(2, PULLUP);
 
  void setup() {
  rotary.report(true);
  rotary.autoDecrease(2000);
+
  }
 
 void loop(){
    rotary.refresh();
+
 }
